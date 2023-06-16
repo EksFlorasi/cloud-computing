967dd1468abcb2f6f8114ae5619262ae9848c8c8
Calista <calista.chan02@gmail.com>
2023-06-16 17:16:47 +0700

    added some changes

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const { initModels } = require('../models/init-model');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
+  host: process.env.INSTANCE_UNIX_SOCKET,
   dialect: 'mysql',
 });
 
