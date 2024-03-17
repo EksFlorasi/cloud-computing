475f463b636355e21735f589fd9fb637c66bb0d6
Calista <calista.chan02@gmail.com>
2024-03-17 15:40:51 +0700

    Add socketPath option in dialectOptions for Cloud SQL connection

--- config/database.js
+++ config/database.js
@@ -3,8 +3,11 @@ const { Sequelize } = require('sequelize');
 const { initModels } = require('../models/init-model');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.INSTANCE_UNIX_SOCKET,
+  host: `/cloudsql/${process.env.INSTANCE}`,
   dialect: 'mysql',
+  dialectOptions: {
+    socketPath: `/cloudsql/${process.env.INSTANCE}`,
+  },
 });
 
 initModels(sequelize);
