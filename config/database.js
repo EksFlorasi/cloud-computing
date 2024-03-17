655d602df1c54554d03d8013c7be63b1d2b44b38
Calista <calista.chan02@gmail.com>
2024-03-17 17:04:50 +0700

    Fix .env variable name in socketPath

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ const { initModels } = require('../models/init-model');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   dialect: 'mysql',
   dialectOptions: {
-    socketPath: `/cloudsql/${process.env.INSTANCE}`,
+    socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
   },
 });
 
