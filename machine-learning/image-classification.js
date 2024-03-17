a168022f3c52a5abfa2a41f81a8b41416dcda7d7
Calista <calista.chan02@gmail.com>
2024-03-17 18:04:39 +0700

    Add absolute URL for path selection branch

--- machine-learning/image-classification.js
+++ machine-learning/image-classification.js
@@ -31,11 +31,11 @@ const imageClassification = async (path, isFlora) => {
 
   // Branching to select paths and image size
   if (isFlora === 'yes') {
-    modelPath = './flora_model/model.json';
+    modelPath = 'file://flora_model/model.json';
     labelPath = './labels/flora_labels.txt';
     imageSize = [225, 225];
   } else {
-    modelPath = './fauna_model/model.json';
+    modelPath = 'file://fauna_model/model.json';
     labelPath = './labels/fauna_labels.txt';
     imageSize = [225, 225];
   }
